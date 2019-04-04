013b9c1ecd9273e492becccb095d1aac9430087c
mciuchitu <xscrub96x@gmail.com>
2019-04-04 12:22:57 +0300

    create empty console application

---
+++ pr-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pr_2
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
